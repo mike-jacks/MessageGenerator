686cac4e6eb6aed2d4f89932d4a923d29c6cb58f
Mike Jacks <mikejacks@me.com>
2020-11-20 21:28:29 -0800

    added magicEightBall.js

---
+++ magicEightBall.js
@@ -0,0 +1,13 @@
+function randomNumber(range) {
+  // gets # from 0 -> range - 1
+  return Math.floor(Math.random() * range);
+}
+
+const answers = {
+  yes: [],
+  no: [],
+  maybe: [],
+  "you bet": [],
+};
+
+function magicEightBall(question) {}
